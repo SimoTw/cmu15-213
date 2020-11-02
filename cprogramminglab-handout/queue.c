19f4b895d171219bcb069bcd9fd4c65bc1bcc28d
simo <simohsiehtw@gmail.com>
2020-11-02 22:47:13 +0800

    pass lab 0

--- cprogramminglab-handout/queue.c
+++ cprogramminglab-handout/queue.c
@@ -24,9 +24,13 @@
 */
 queue_t *q_new()
 {
-    queue_t *q =  malloc(sizeof(queue_t));
+    queue_t *q =  (queue_t *)malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
+    if (q == NULL) return NULL;
     q->head = NULL;
+
+    q->tail = NULL;
+    q->length = 0;
     return q;
 }
 
@@ -35,6 +39,14 @@ void q_free(queue_t *q)
 {
     /* How about freeing the list elements and the strings? */
     /* Free queue structure */
+    list_ele_t *node, *prev;
+    node = q->head;
+    while (node != NULL) {
+      free(node->value);
+      prev = node;
+      node = node->next;
+      free(prev);
+    }
     free(q);
 }
 
@@ -47,13 +59,29 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
-    list_ele_t *newh;
     /* What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
+    // create new q struct if it's null
+    if (q == NULL) {
+      return false;
+    }
+    list_ele_t *node;
+    // alloc memory for new node
+    node = (list_ele_t *)malloc(sizeof(list_ele_t));
+    if (node == NULL) return false;
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
-    newh->next = q->head;
-    q->head = newh;
+
+    // alloce and copy string list
+    node->value = (char *)malloc(strlen(s)+1);
+    if (node->value == NULL) return false;
+    strcpy(node->value, s);
+
+    // insert the new node to the queue
+    node->next = q->head;
+    q->head = node;
+    if (q->tail == NULL) q->tail = node;
+  
+    q->length += 1;
     return true;
 }
 
@@ -69,7 +97,35 @@ bool q_insert_tail(queue_t *q, char *s)
 {
     /* You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
-    return false;
+    /* What should you do if the q is NULL? */
+    // create new queue struct if it's null
+    if (q == NULL) {
+      return false;
+    }
+    // alloc new node
+    list_ele_t *node;
+    node = (list_ele_t *)malloc(sizeof(list_ele_t));
+    if (node == NULL) {
+      return false;
+    }
+    /* Don't forget to allocate space for the string and copy it */
+    /* What if either call to malloc returns NULL? */
+
+    // copy string
+    node->value = (char *)malloc(strlen(s)+1);
+    if (node->value == NULL) return false;
+    strcpy(node->value, s);
+    node->next = NULL;
+    // insert node
+    if (q->tail == NULL) {
+      q->head = node;
+      q->tail = node;
+    } else {
+      q->tail->next = node;
+      q->tail = node;
+    }
+    q->length += 1;
+    return true;
 }
 
 /*
@@ -83,7 +139,23 @@ bool q_insert_tail(queue_t *q, char *s)
 bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* You need to fix up this code. */
-    q->head = q->head->next;
+    if (q->length == 0) {
+      return false;
+    } else if (q->length == 1) {
+      list_ele_t *temp;
+      temp = q->head;
+      free(q->head->value);
+      q->head = NULL;
+      q->tail = NULL;
+      free(temp);
+      q->length = 0;
+    } else {
+      list_ele_t *temp;
+      temp = q->head;
+      free(q->head->value);
+      q->head = q->head->next;
+      free(temp);
+    }
     return true;
 }
 
@@ -95,7 +167,7 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-    return 0;
+    return q->length;
 }
 
 /*
@@ -107,6 +179,24 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
+      // add a head to the new q
+
+      // track the curr node
+      // append curr node to the head of the queue
+      // move to 
+
+    list_ele_t *prev, *curr, *next;
+    prev = NULL;
+    next = NULL;
+    curr = q->head;
+    q->tail = q->head;
+    while (curr != NULL) {
+      next = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = next;
+    }
+    q->head = prev;
     /* You need to write the code for this function */
 }
 
